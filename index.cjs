bdb205b0e5580cd459760fc2cf7ad105085ed55b
Saucawalka <164861125+Saucawalka@users.noreply.github.com>
2025-05-01 19:45:41 +0100

    vercel1

--- index.cjs
+++ index.cjs
@@ -25,12 +25,12 @@ const categoryRoutes = require('./routes/category.cjs');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json( {limit: '10mb'}));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-app.use(cors({origin :'https://saucytee-yvww.vercel.app/',credentials:true}));
+app.use(cors({origin :'https://saucytee-l5xk.vercel.app/',credentials:true}));
 
 // JWT Middleware
 const verifyToken = (req, res, next) => {
